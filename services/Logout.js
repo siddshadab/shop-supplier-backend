5db22847e4c9d48759a71d5a3d391b9f4d2f7f21
Shadab Siddiqui <shadab.siddiqui@Infraseepz.lan>
2021-04-17 02:33:46 +0530

    first commit

---
+++ services/Logout.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+// const User = require('../models/User');
+const UserSession = require('../models/UserSession');
+
+router.get("/logout", function(req, res, next){
+ 
+    UserSession.findOneAndUpdate({_id:req.query.token, isDeleted:false}, {$set:{isDeleted:true}} , null, function(error, sessions){
+        if(error){
+            return res.send({success: false, message: "Error: Server error"});
+        }
+            return res.send({success: true, message: 'UserSession isDeleted to true'});
+    });
+}
+
+module.exports = router;
